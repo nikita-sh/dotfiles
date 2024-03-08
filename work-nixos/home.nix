cf1b521ceb4a47d8a810788af95159743e0dea2e
Nikita Shumeiko <nikita.shumeiko@vitalbio.com>
2024-03-08 10:50:52 -0500

    update p10k config

--- work-nixos/home.nix
+++ work-nixos/home.nix
@@ -173,10 +173,21 @@
             name = "powerlevel10k";
             src = pkgs.zsh-powerlevel10k;
             file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
-          } 
+          }
+          {
+            name = "zsh-nix-shell";
+            file = "nix-shell.plugin.zsh";
+            src = pkgs.fetchFromGitHub {
+              owner = "chisui";
+              repo = "zsh-nix-shell";
+              rev = "v0.8.0";
+              sha256 = "1lzrn0n4fxfcgg65v0qhnj7wnybybqzs4adz7xsrkgmcsr0ii8b7";
+            };
+          }
       ];
       initExtra = ''
         source /home/nikita/dev/nix-env/dot/dot-p10k.zsh
+        prompt_nix_shell_setup
       '';
     #   initExtra = ''
     #     # Powerlevel10k Zsh theme  
