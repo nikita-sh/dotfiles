d209941a021ce68627c272c7998deb5f801d3507
Nikita Shumeiko <nikita.shumeiko@vitalbio.com>
2026-04-03 16:42:17 -0400

    move kolibri system cfg to separate file

---
+++ systems/kolibri/config.nix
@@ -0,0 +1,118 @@
+{ pkgs, ... }:
+{
+  boot.loader = {
+    systemd-boot.enable = true;
+    efi.canTouchEfiVariables = true;
+    systemd-boot.configurationLimit = 10;
+  };
+
+  networking.hostName = "kolibri";
+
+  programs = {
+    dconf.enable = true;
+    zsh.enable = true;
+    ssh.startAgent = true;
+  };
+
+  services = {
+    dbus.enable = true;
+    openssh.enable = true;
+    tailscale.enable = true;
+    postgresql = {
+      enable = true;
+      authentication = pkgs.lib.mkOverride 10 ''
+        #type database  DBuser  auth-method
+        local all       all     trust
+      '';
+      ensureUsers = [
+        {
+          name = "nikita";
+          ensureClauses.superuser = true;
+        }
+      ];
+      ensureDatabases = [
+        "conductor"
+      ];
+    };
+  };
+
+  nix = {
+    settings = {
+      download-buffer-size = "100000000";
+      trusted-users = [ "nikita" ];
+      auto-optimise-store = true;
+      experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
+    };
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 7d";
+    };
+    distributedBuilds = true;
+    extraOptions = ''
+      builders-use-substitutes = true
+      keep-outputs = true
+      keep-derivations = true
+      netrc-file = /etc/netrc
+    '';
+    buildMachines = [
+      {
+        hostName = "nixbuild.vital.company";
+        system = "x86_64-linux";
+        maxJobs = 64;
+        speedFactor = 2;
+        sshUser = "nikita";
+        sshKey = "/home/nikita/.ssh/id_ed25519";
+        supportedFeatures = [
+          "benchmark"
+          "big-parallel"
+        ];
+      }
+      {
+        hostName = "nixbuild.vital.company";
+        system = "aarch64-linux";
+        maxJobs = 64;
+        speedFactor = 2;
+        sshUser = "nikita";
+        sshKey = "/home/nikita/.ssh/id_ed25519";
+        supportedFeatures = [
+          "benchmark"
+          "big-parallel"
+        ];
+      }
+      {
+        hostName = "hydra-aarch64.vital.company";
+        system = "aarch64-linux";
+        maxJobs = 64;
+        speedFactor = 2;
+        sshUser = "nikita";
+        supportedFeatures = [
+          "benchmark"
+          "big-parallel"
+        ];
+      }
+      {
+        hostName = "hydra-x86-64.vital.company";
+        system = "x86_64-linux";
+        maxJobs = 64;
+        speedFactor = 2;
+        sshUser = "nikita";
+        supportedFeatures = [
+          "benchmark"
+          "big-parallel"
+        ];
+      }
+    ];
+  };
+
+  security = {
+    rtkit.enable = true;
+    sudo = {
+      enable = true;
+      wheelNeedsPassword = false;
+    };
+  };
+}
