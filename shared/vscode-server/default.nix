a145a3a8df348f1f70e239cea80c259a37d09cce
Nikita Shumeiko <dev@nikitashko.com>
2026-04-05 15:25:33 -0400

    update vscode-server

--- shared/vscode-server/default.nix
+++ shared/vscode-server/default.nix
@@ -4,7 +4,7 @@
   services.vscode-server.enable = true;
 
   home.file.".vscode-server/data/Machine/settings.json".text = builtins.toJSON {
-    "claude.executablePath" = "${pkgs.claude-code}/bin/claude";
+    "claudeCode.claudeProcessWrapper" = "${pkgs.claude-code}/bin/claude";
     "nix.enableLanguageServer" = true;
     "direnv.path.executable" = "direnv";
   };
