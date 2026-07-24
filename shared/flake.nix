be478a3b7676888c292d7e1af3d1f249f3d07875
Nikita Shumeiko <nikita@mercury.com>
2026-07-24 10:30:06 -0400

    add shared/jj

--- shared/flake.nix
+++ shared/flake.nix
@@ -38,6 +38,7 @@
         ./direnv
         ./git
         ./home-manager
+        ./jj
         ./lsd
         ./nvim
         ./packages
---
+++ shared/jj/default.nix
@@ -0,0 +1,12 @@
+{ ... }:
+{
+  programs.jujutsu = {
+    enable = true;
+    settings = {
+      user = {
+        name = "Nikita Shumeiko";
+        email = "nikita@mercury.com";
+      };
+    };
+  };
+}
